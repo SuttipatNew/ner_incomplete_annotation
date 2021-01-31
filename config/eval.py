b748949df5eca9b2c4741265058603725c3c8a4c
Suttipat Sukwattanakorn <suttipat@wongnai.com>
2021-02-01 00:08:23 +0700

    fix evaluation

--- config/eval.py
+++ config/eval.py
@@ -28,63 +28,115 @@ class Span:
         return hash((self.left, self.right, self.type))
 
 
+# def evaluate_batch_insts(batch_insts: List[Instance],
+#                          batch_pred_ids: torch.Tensor,
+#                          batch_gold_ids: torch.LongTensor,
+#                          word_seq_lens: torch.LongTensor,
+#                          idx2label: List[str]) -> np.ndarray:
+#     """
+#     Evaluate a batch of instances and handling the padding positions.
+#     :param batch_insts:  a batched of instances.
+#     :param batch_pred_ids: Shape: (batch_size, max_length) prediction ids from the viterbi algorithm.
+#     :param batch_gold_ids: Shape: (batch_size, max_length) gold ids.
+#     :param word_seq_lens: Shape: (batch_size) the length for each instance.
+#     :param idx2label: The idx to label mapping.
+#     :return: numpy array containing (number of true positive, number of all positive, number of true positive + number of false negative)
+#              You can also refer as (number of correctly predicted entities, number of entities predicted, number of entities in the dataset)
+#     """
+#     p = 0
+#     total_entity = 0
+#     total_predict = 0
+#     word_seq_lens = word_seq_lens.tolist()
+#     for idx in range(len(batch_pred_ids)):
+#         length = word_seq_lens[idx]
+#         output = batch_gold_ids[idx][:length].tolist()
+#         prediction = batch_pred_ids[idx][:length].tolist()
+#         prediction = prediction[::-1]
+#         output = [idx2label[l] for l in output]
+#         prediction =[idx2label[l] for l in prediction]
+#         batch_insts[idx].prediction = prediction
+#         #convert to span
+#         output_spans = set()
+#         start = -1
+#         for i in range(len(output)):
+#             if output[i].startswith("B-"):
+#                 start = i
+#             if output[i].startswith("E-"):
+#                 end = i
+#                 output_spans.add(Span(start, end, output[i][2:]))
+#             if output[i].startswith("S-"):
+#                 output_spans.add(Span(i, i, output[i][2:]))
+#         predict_spans = set()
+#         start = -1
+#         for i in range(len(prediction)):
+#             if prediction[i].startswith("B-"):
+#                 start = i
+#             if prediction[i].startswith("E-"):
+#                 end = i
+#                 predict_spans.add(Span(start, end, prediction[i][2:]))
+#             if prediction[i].startswith("S-"):
+#                 predict_spans.add(Span(i, i, prediction[i][2:]))
+
+#         total_entity += len(output_spans)
+#         total_predict += len(predict_spans)
+#         p += len(predict_spans.intersection(output_spans))
+
+#     # In case you need the following code for calculating the p/r/f in a batch.
+#     # (When your batch is the complete dataset)
+#     # precision = p * 1.0 / total_predict * 100 if total_predict != 0 else 0
+#     # recall = p * 1.0 / total_entity * 100 if total_entity != 0 else 0
+#     # fscore = 2.0 * precision * recall / (precision + recall) if precision != 0 or recall != 0 else 0
+
+#     return np.asarray([p, total_predict, total_entity], dtype=int)
+
 def evaluate_batch_insts(batch_insts: List[Instance],
                          batch_pred_ids: torch.Tensor,
                          batch_gold_ids: torch.LongTensor,
                          word_seq_lens: torch.LongTensor,
                          idx2label: List[str]) -> np.ndarray:
-    """
-    Evaluate a batch of instances and handling the padding positions.
-    :param batch_insts:  a batched of instances.
-    :param batch_pred_ids: Shape: (batch_size, max_length) prediction ids from the viterbi algorithm.
-    :param batch_gold_ids: Shape: (batch_size, max_length) gold ids.
-    :param word_seq_lens: Shape: (batch_size) the length for each instance.
-    :param idx2label: The idx to label mapping.
-    :return: numpy array containing (number of true positive, number of all positive, number of true positive + number of false negative)
-             You can also refer as (number of correctly predicted entities, number of entities predicted, number of entities in the dataset)
-    """
     p = 0
     total_entity = 0
     total_predict = 0
     word_seq_lens = word_seq_lens.tolist()
+
     for idx in range(len(batch_pred_ids)):
         length = word_seq_lens[idx]
         output = batch_gold_ids[idx][:length].tolist()
         prediction = batch_pred_ids[idx][:length].tolist()
         prediction = prediction[::-1]
+
         output = [idx2label[l] for l in output]
         prediction =[idx2label[l] for l in prediction]
         batch_insts[idx].prediction = prediction
-        #convert to span
+        
+        # convert to span
         output_spans = set()
-        start = -1
+        named_entity = []
         for i in range(len(output)):
+            if (output[i].startswith('B-') or output[i] == 'O') and len(named_entity) > 0:
+                output_spans.add(tuple(named_entity))
+                named_entity = []
             if output[i].startswith("B-"):
-                start = i
-            if output[i].startswith("E-"):
-                end = i
-                output_spans.add(Span(start, end, output[i][2:]))
-            if output[i].startswith("S-"):
-                output_spans.add(Span(i, i, output[i][2:]))
+                named_entity.append(i)
+            if output[i].startswith("I-") and len(named_entity) > 0:
+                named_entity.append(i)
+        if len(named_entity) > 0:
+            named_entity.append(tuple(named_entity))
+        
         predict_spans = set()
-        start = -1
+        named_entity = []
         for i in range(len(prediction)):
+            if (prediction[i].startswith('B-') or prediction[i] == 'O') and len(named_entity) > 0:
+                output_spans.add(tuple(named_entity))
+                named_entity = []
             if prediction[i].startswith("B-"):
-                start = i
-            if prediction[i].startswith("E-"):
-                end = i
-                predict_spans.add(Span(start, end, prediction[i][2:]))
-            if prediction[i].startswith("S-"):
-                predict_spans.add(Span(i, i, prediction[i][2:]))
+                named_entity.append(i)
+            if prediction[i].startswith("I-") and len(named_entity) > 0:
+                named_entity.append(i)
+        if len(named_entity) > 0:
+            named_entity.append(tuple(named_entity))
 
         total_entity += len(output_spans)
         total_predict += len(predict_spans)
         p += len(predict_spans.intersection(output_spans))
-
-    # In case you need the following code for calculating the p/r/f in a batch.
-    # (When your batch is the complete dataset)
-    # precision = p * 1.0 / total_predict * 100 if total_predict != 0 else 0
-    # recall = p * 1.0 / total_entity * 100 if total_entity != 0 else 0
-    # fscore = 2.0 * precision * recall / (precision + recall) if precision != 0 or recall != 0 else 0
-
     return np.asarray([p, total_predict, total_entity], dtype=int)
